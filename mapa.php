221360c99f595a9d6d9248494aff98381091143f
Jakoppo <profiljakub@gmail.com>
2024-09-11 10:25:55 +0200

    map added

---
+++ mapa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+<img src="Viego_21.jpg" alt="Workplace" usemap="#workmap" width="500" height="400">
+    <map name="workmap">
+    <area shape="rect" coords="200,50,300,70" alt="crown" href="https://www.leagueoflegends.com/pl-pl/champions/viego/">
+    </map>
+</body>
+</html>
